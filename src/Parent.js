91dd28bed603948f348105cb5d2b650935d4db35
asgharlatif <asgharlatif@hotmail.com>
2021-01-02 13:24:20 -0500

    initial commit

---
+++ src/Parent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Child from './Child'
+
+function Parent(props) {
+  return (
+    <div>
+        Parent {props.num}
+        <Child num={props.num} ></Child>
+    </div>
+  );
+}
+
+export default Parent;
